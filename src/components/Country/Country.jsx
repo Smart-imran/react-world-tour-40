27e041a3421b1be2bfd332cc281fd3160fb7e8b3
Smart-imran <rst.imran4@gmail.com>
2024-08-31 00:26:18 +0600

    country flag

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,17 @@
 import './Country.css'
-const Country = ({country}) => {
-    console.log(country);
-   
-    const {name,flags} = country;
 
+const Country = ({country}) => {
+    
+    console.log(country)
+
+    const {name, flags, population, cca3} = country
     return (
         <div className="country">
-            <h3>Name:{name?.common}</h3>
+            <h3>Name: {name.common}</h3>
+            <h3>Population: {population}</h3>
+            <p><small>Code: {cca3} </small></p>
             <img src={flags.png} alt="" />
-            
+            <button>Visited</button>
         </div>
     );
 };
